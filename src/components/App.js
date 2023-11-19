3fdf2a1acd08b2eeb2cd1e6a28421ed38cbdc967
Edgar Otieno <eddyade98@gmail.com>
2023-11-19 14:12:06 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -11,18 +11,27 @@ function NavBar() {
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h1>Home</h1> 
     </div>
   );
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2> 
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
